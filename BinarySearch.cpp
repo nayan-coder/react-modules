3066d6b6285fc8162a763a9655a789949b9288ea
kunal-7813 <73578471+kunal-7813@users.noreply.github.com>
2022-10-12 20:07:25 +0530

    Create BinarySearch.cpp

---
+++ BinarySearch.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int binarySearch(int arr[], int l, int r, int x)
+{
+    if (r >= l) {
+        int mid = l + (r - l) / 2;
+ 
+        if (arr[mid] == x)
+            return mid;
+ 
+        if (arr[mid] > x)
+            return binarySearch(arr, l, mid - 1, x);
+ 
+        return binarySearch(arr, mid + 1, r, x);
+    }
+ 
+    return -1;
+}
+ 
+int main(void)
+{
+    int arr[] = { 5, 73, 12, 532, 1211, 22 };
+    int x = 22;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int result = binarySearch(arr, 0, n - 1, x);
+    (result == -1)
+        ? cout << "Element is not present in array"
+        : cout << "Element is present at index " << result;
+    return 0;
+}
