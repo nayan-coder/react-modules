9f6a8527bfac3642fac41d831a654567c9797a94
Ninja-2408 <116113893+Ninja-2408@users.noreply.github.com>
2022-10-19 00:33:53 +0530

    Create Bucket_Sort.cpp

---
+++ Bucket_Sort.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+  
+void bucketSort(float arr[], int n)
+{
+      vector<float> b[n];
+  
+    for (int i = 0; i < n; i++) {
+        int bi = n * arr[i]; // Index in bucket
+        b[bi].push_back(arr[i]);
+    }
+  
+    for (int i = 0; i < n; i++)
+        sort(b[i].begin(), b[i].end());
+  
+    int index = 0;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < b[i].size(); j++)
+            arr[index++] = b[i][j];
+}
+  
+int main()
+{
+    float arr[] = { 0.917, 0.888, 0.412, 0.124, 0.325, 0.313 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    bucketSort(arr, n);
+  
+    cout << "Sorted array is \n";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    return 0;
+}
